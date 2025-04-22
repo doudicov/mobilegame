7dd65a755fd1d66191efbfef4c94a3c6af06707d
doudicov <115922881+doudicov@users.noreply.github.com>
2025-04-22 23:49:10 +0200

    Damage
    Take damage amount , Death trigger, Death Transition, custom state behaviour for Death state, vanish after death

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -96,6 +96,14 @@ public class PlayerController : MonoBehaviour
             return animator.GetBool(AnimationStrings.canMove);
         } }
 
+    public bool IsAlive
+    {
+        get
+        {
+            return(animator.GetBool(AnimationStrings.isAlive));
+        }
+    }
+
     Rigidbody2D rb;
     Animator animator;
     private bool isFacingRight;
@@ -119,9 +127,18 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = context.ReadValue<Vector2>();
 
-        IsMoving = moveInput !=Vector2.zero;
+        if(IsAlive)
+        {
+            IsMoving = moveInput != Vector2.zero;
 
-        SetFacingDirection(moveInput);
+            SetFacingDirection(moveInput);
+        }
+        else
+        {
+            IsMoving = false;
+        }
+
+        
     }
 
     private void SetFacingDirection(Vector2 moveInput)
