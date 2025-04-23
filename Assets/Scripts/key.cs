33907f05dd062aef456649bed48c8d57101d24de
doudicov <115922881+doudicov@users.noreply.github.com>
2025-04-23 10:43:14 +0200

    door , key
     door and key and continuation for torch

---
+++ Assets/Scripts/key.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
