5a8d76c6df0536351f480bcfb947121c856cc74a
doudicov <115922881+doudicov@users.noreply.github.com>
2025-04-23 06:59:52 +0200

    more attacks
    attack 2, attack 3, transition time , Scaling damage , combo attack

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Damageable))]
 public class Skeleton : MonoBehaviour
 {
-    public float walkspeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxspeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -99,7 +100,11 @@ public class Skeleton : MonoBehaviour
         if (!damageable.LockVelocity)
         {
             if (CanMove)
-                rb.velocity = new Vector2(walkspeed * walkDirectionVector.x, rb.velocity.y);
+
+                rb.velocity = new Vector2(
+                    Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), -maxspeed, maxspeed),
+                    rb.velocity.y);
+
             else
                 rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
