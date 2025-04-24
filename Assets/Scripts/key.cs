93d819fcf4a11e076ec7d1af8e33cf9a6ac1b0a9
doudicov <115922881+doudicov@users.noreply.github.com>
2025-04-24 19:28:27 +0200

    sound effects
    sound effects for player and skeleton attacks, sound effect for game environment , sound effect for picking up key , door options (not working, not finalized door look)

--- Assets/Scripts/key.cs
+++ Assets/Scripts/key.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class key : MonoBehaviour
 {
+    AudioSource pickupSource;
+
+    private void Awake()
+    {
+        pickupSource = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.gameObject.CompareTag("Player"))
         {
             gameObject.SetActive(false);
+            if (pickupSource)
+                AudioSource.PlayClipAtPoint(pickupSource.clip, gameObject.transform.position, pickupSource.volume);
 
         }
 
